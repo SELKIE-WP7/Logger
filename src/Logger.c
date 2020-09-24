c5d0c127c93c5fbb9e16f07be3feca1c32f68b1d
Thomas Lake <t.lake@swansea.ac.uk>
2020-09-24 12:35:10 +0100

    Remove extra . in file extension

--- src/Logger.c
+++ src/Logger.c
@@ -135,7 +135,7 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	FILE *monitorFile = openSerialNumberedFile(monPrefix, ".dat");
+	FILE *monitorFile = openSerialNumberedFile(monPrefix, "dat");
 
 	if (monitorFile == NULL) {
 		perror("monitorFile");

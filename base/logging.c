05126885e50c18fff35344f569b7e8c6ffc8e864
Thomas Lake <t.lake@swansea.ac.uk>
2020-09-25 12:23:36 +0100

    Add logging support functions

---
+++ base/logging.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <time.h>
+#include <errno.h>
+
+#include "logging.h"
+
+/*!
+ * \addtogroup logging  Logging support functions
+ * @{
+ */
+
+void log_error(const program_state *s, const char *format, ...) {
+	va_list args;
+	char *label;
+	if (s->shutdown) {
+		label="[Shutdown]";
+	} else if (!s->started) {
+		label="[Startup]";
+	} else {
+		label="";
+	}
+	fprintf(stderr, "%s Error: ", label);
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+	fprintf(stderr, "\n");
+
+	if (s->log) {
+		fprintf(s->log, "%s Error: ", label);
+		va_start(args, format);
+		vfprintf(s->log, format, args);
+		va_end(args);
+		fprintf(s->log, "\n");
+	}
+}
+
+void log_warning(const program_state *s, const char *format, ...) {
+	va_list args;
+	char *label;
+	if (s->shutdown) {
+		label="[Shutdown]";
+	} else if (!s->started) {
+		label="[Startup]";
+	} else {
+		label="";
+	}
+	fprintf(stderr, "%s Warning: ", label);
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+	fprintf(stderr, "\n");
+
+	if (s->log) {
+		fprintf(s->log, "%s Error: ", label);
+		va_start(args, format);
+		vfprintf(s->log, format, args);
+		va_end(args);
+		fprintf(s->log, "\n");
+	}
+}
+
+void log_info(const program_state *s, const int level, const char *format, ...) {
+	va_list args;
+	char *label;
+	if (s->shutdown) {
+		label="[Shutdown]";
+	} else if (!s->started) {
+		label="[Startup]";
+	} else {
+		label="";
+	}
+
+	if (s->verbose >= level) {
+		fprintf(stdout, "%s Info:%d: ", label, level);
+		va_start(args, format);
+		vfprintf(stdout, format, args);
+		va_end(args);
+		fprintf(stdout, "\n");
+	}
+
+	if (s->log && (s->logverbose >= level)) {
+		fprintf(s->log, "%s Info:%d: ", label, level);
+		va_start(args, format);
+		vfprintf(s->log, format, args);
+		va_end(args);
+		fprintf(s->log, "\n");
+	}
+}
+
+FILE *openSerialNumberedFile(const char *prefix, const char *extension) {
+	time_t now = time(NULL);
+	struct tm *tm = localtime(&now);
+	char *fileName = NULL;
+	char date[9];
+	strftime(date, 9, "%Y%m%d", tm);
+
+	int i = 0;
+	FILE *file = NULL;
+	while (i <= 0xFF) {
+		errno = 0;
+		if (asprintf(&fileName, "%s%s%02x.%s", prefix, date, i, extension) == -1) {
+			return NULL;
+		}
+		errno = 0;
+		file = fopen(fileName, "w+x"); //w+x = rw + create. Fail if exists
+		if (file) {
+			fprintf(stdout, "Using: %s.\n", fileName);
+			free(fileName);
+			return file;
+		}
+		if (errno != EEXIST) {
+			free(fileName);
+			return NULL;
+		}
+		i++;
+		free(fileName);
+		fileName = NULL;
+	}
+
+	return NULL;
+}
+
+//!@}
---
+++ base/logging.h
@@ -0,0 +1,44 @@
+#ifndef SELKIELogging
+#define SELKIELogging
+#include <stdbool.h>
+
+/*!
+ * \addtogroup Logging Logging Support functions
+ *
+ * @{
+ */
+
+//! Program state and logging information
+typedef struct {
+	bool started; //!< Indicates startup completed
+	bool shutdown; //!< Indicates shutdown begun
+	int verbose; //!< Current log verbosity (console output)
+	FILE *log; //!< Log file
+	int logverbose; //!< Current log verbosity (file output)
+} program_state;
+
+//! Output formatted error message
+void log_error(const program_state *s, const char *format, ...) __attribute__ ((format (gnu_printf, 2, 3)));
+
+//! Output formatted warning message
+void log_warning(const program_state *s, const char *format, ...) __attribute__ ((format (gnu_printf, 2, 3)));
+
+//! Output formatted information messsage at a given level
+void log_info(const program_state *s, const int level, const char *format, ...) __attribute__ ((format (gnu_printf, 3, 4)));
+
+/*! @brief Open dated, serial numbered file with given prefix and extension
+ *
+ * Generates a file name with the form [prefix]YYYYMMDDXX.[extension], where XX
+ * is a two digit hexadecimal serial number. Starting from 0, the proposed file
+ * name is opened in exclusive create mode ("w+x"). If this fails because the
+ * file exists, the serial number is incremented and another attempt to create
+ * the file is made until either a) an unused serial number is found or b) the
+ * serial number reaches 0xFF.
+ *
+ * If no valid file name can be generated, or if any other error is
+ * encountered, the function returns NULL.
+ */
+FILE *openSerialNumberedFile(const char *prefix, const char *extension);
+
+//!@}
+#endif

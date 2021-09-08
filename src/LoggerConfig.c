6f431a8ad3cf02427560779707ff16579c74c67d
Thomas Lake <t.lake@swansea.ac.uk>
2021-09-08 16:36:02 +0100

    Add config search functions

--- src/LoggerConfig.c
+++ src/LoggerConfig.c
@@ -25,7 +25,7 @@ int config_handler(void* user, const char* section, const char* name, const char
 			config_section *a = realloc(c->sects,c->sectsize * sizeof(config_section));
 			if (a == NULL) {
 				perror("config_handler");
-				return -99;
+				return 0;
 			}
 			c->sects = a;
 			memset(&(c->sects[c->numsects]), 0, sizeof(config_section) * 5);
@@ -50,7 +50,7 @@ int config_handler(void* user, const char* section, const char* name, const char
 
 		if (a == NULL) {
 			perror("config_handler");
-			return -99;
+			return 0;
 		}
 		cs->opts = a;
 		memset(&(cs->opts[cs->numopts]), 0, sizeof(config_kv) * 5);
@@ -59,7 +59,7 @@ int config_handler(void* user, const char* section, const char* name, const char
 	k->key = strdup(name);
 	k->value = strdup(value);
 
-	return 0;
+	return 1;
 }
 
 bool new_config(ini_config *c) {
@@ -117,7 +117,7 @@ void print_config(ini_config *c) {
 
 	for (int i = 0; i < c->numsects; ++i) {
 		config_section *cs = &(c->sects[i]);
-		if (strcmp("", cs->name)) {
+		if (strcmp("", cs->name) != 0) {
 			fprintf(stdout, "[%s]\n", cs->name);
 		}
 		for (int j = 0; j < cs->numopts; ++j) {
@@ -125,4 +125,54 @@ void print_config(ini_config *c) {
 		}
 		fprintf(stdout, "\n");
 	}
+}
+
+config_section *config_get_section(const ini_config *in, const char *sn) {
+	for (int i = 0; i < in->numsects; i++) {
+		config_section *cs = &(in->sects[i]);
+		if (strcasecmp(sn, cs->name) == 0) {
+			return cs;
+		}
+	}
+	return NULL;
+}
+
+config_kv *config_get_key(const config_section *cs, const char *kn) {
+	for (int i = 0; i < cs->numopts; i++) {
+		config_kv *k = &(cs->opts[i]);
+		if (strcasecmp(kn, k->key) == 0) {
+			return k;
+		}
+	}
+	return NULL;
+}
+
+config_kv *config_get_option(const ini_config *in, const char *sn, const char *kn) {
+	config_section *cs = config_get_section(in, sn);
+	if (cs == NULL) {
+		return NULL;
+	}
+	return config_get_key(cs, kn);
+}
+
+int config_parse_bool(const char *b) {
+	if (b == NULL || (strcmp(b, "") == 0)) {
+		return -1;
+	}
+
+	switch (b[0]) {
+		case 'Y':
+		case 'y':
+		case 'T':
+		case 't':
+		case '1':
+			return 1;
+		case 'N':
+		case 'n':
+		case 'F':
+		case 'f':
+		case '0':
+			return 0;
+	}
+	return -1;
 }
--- src/LoggerConfig.h
+++ src/LoggerConfig.h
@@ -34,4 +34,12 @@ bool new_config(ini_config *c);
 void destroy_config(ini_config *c);
 
 void print_config(ini_config *c);
+
+config_section *config_get_section(const ini_config *in, const char *sn);
+
+config_kv *config_get_key(const config_section *cs, const char *kn);
+
+config_kv *config_get_option(const ini_config *in, const char *sn, const char *kn);
+
+int config_parse_bool(const char *b);
 #endif

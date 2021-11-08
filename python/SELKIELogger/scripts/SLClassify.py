bfdd50d254ac7acfe192bd82a869824370c80ccf
Thomas Lake <t.lake@swansea.ac.uk>
2021-11-08 15:41:13 +0000

    Refactor SLClassify script before expanding

--- python/SELKIELogger/scripts/SLClassify.py
+++ python/SELKIELogger/scripts/SLClassify.py
@@ -10,33 +10,32 @@ def process_arguments():
     options.add_argument("-v, --verbose", dest="verbose", action="store_true", default=False, help="Include timing and source/channel information messages")
     return options.parse_args()
 
-def SLClassify():
-    logging.basicConfig(level=logging.INFO)
-    args = process_arguments()
-    inFile = open(args.file, "rb")
-    unpacker = msgpack.Unpacker(inFile, unicode_errors='ignore')
+class ProcessedRecords():
+    def __init__(self):
+        self.SourceTypes = ["GPS", "NMEA", "MP", "Total"]
+        self.classes = {x: {} for x in self.SourceTypes}
+        self.processed = {x: 0 for x in self.SourceTypes}
+
+def classify_messages(file):
+    unpacker = msgpack.Unpacker(file, unicode_errors='ignore')
     out = SLMessageSink(msglogger=logging.getLogger("Messages"))
 
+    tots = ProcessedRecords()
+
     # Dict of Dicts gpsSeen[class][msg] = count and similar for NMEA
-    gpsSeen = {}
-    nmeaSeen = {}
-    mpSeen = {}
-    gpsProcessed = 0
-    nmeaProcessed = 0
-    mpProcessed = 0
     for msg in unpacker:
         msg = out.Process(msg, output="raw")
         if msg is None:
             continue
-        mpProcessed += 1
-        if msg.SourceID in mpSeen:
-            if msg.ChannelID in mpSeen[msg.SourceID]:
-                mpSeen[msg.SourceID][msg.ChannelID] += 1
+        tots.processed["MP"] += 1
+        if msg.SourceID in tots.classes["MP"]:
+            if msg.ChannelID in tots.classes["MP"][msg.SourceID]:
+                tots.classes["MP"][msg.SourceID][msg.ChannelID] += 1
             else:
-                mpSeen[msg.SourceID][msg.ChannelID] = 1
+                tots.classes["MP"][msg.SourceID][msg.ChannelID] = 1
         else:
-            mpSeen[msg.SourceID] = {}
-            mpSeen[msg.SourceID][msg.ChannelID] = 1
+            tots.classes["MP"][msg.SourceID] = {}
+            tots.classes["MP"][msg.SourceID][msg.ChannelID] = 1
 
         if msg.SourceID >= 0x10 and msg.SourceID < 0x20:
             # Assume GPS
@@ -44,26 +43,26 @@ def SLClassify():
                 # We're only looking at raw messages on Ch 3
                 if not msg.ChannelID in [0,1,2]:
                     # But we will count messages that have been converted to other types
-                    gpsProcessed = gpsProcessed + 1
+                    tots.processed["GPS"] = tots.processed["GPS"] + 1
                 continue
             header = int.from_bytes(msg.Data[0:2], byteorder='big', signed=False)
             ubxClass = msg.Data[2]
             ubxMsg = msg.Data[3]
 
-            if ubxClass in gpsSeen:
-                if ubxMsg in gpsSeen[ubxClass]:
-                    gpsSeen[ubxClass][ubxMsg] = gpsSeen[ubxClass][ubxMsg] + 1
+            if ubxClass in tots.classes["GPS"]:
+                if ubxMsg in tots.classes["GPS"][ubxClass]:
+                    tots.classes["GPS"][ubxClass][ubxMsg] = tots.classes["GPS"][ubxClass][ubxMsg] + 1
                 else:
-                    gpsSeen[ubxClass][ubxMsg] = 1
+                    tots.classes["GPS"][ubxClass][ubxMsg] = 1
             else:
-                gpsSeen[ubxClass] = {}
-                gpsSeen[ubxClass][ubxMsg] = 1
+                tots.classes["GPS"][ubxClass] = {}
+                tots.classes["GPS"][ubxClass][ubxMsg] = 1
 
         elif msg.SourceID >= 0x30 and msg.SourceID < 0x40:
             # Assume NMEA
             if msg.ChannelID != 0x03:
                 if not msg.ChannelID in [0,1,2]:
-                    nmeaProcessed = nmeaProcessed + 1
+                    tots.processed["NMEA"] = tots.processed["NMEA"] + 1
                 continue
             nmeaHead = msg.Data[0:10].decode('ascii', 'replace')
             if nmeaHead[1] == "P":
@@ -73,36 +72,46 @@ def SLClassify():
                 talker = nmeaHead[1:3]
                 message = nmeaHead[3:6]
 
-            if talker in nmeaSeen:
-                if message in nmeaSeen[talker]:
-                    nmeaSeen[talker][message] = nmeaSeen[talker][message] + 1
+            if talker in tots.classes["NMEA"]:
+                if message in tots.classes["NMEA"][talker]:
+                    tots.classes["NMEA"][talker][message] = tots.classes["NMEA"][talker][message] + 1
                 else:
-                    nmeaSeen[talker][message] = 1
+                    tots.classes["NMEA"][talker][message] = 1
             else:
-                nmeaSeen[talker] = {}
-                nmeaSeen[talker][message] = 1
+                tots.classes["NMEA"][talker] = {}
+                tots.classes["NMEA"][talker][message] = 1
+
+    return tots
 
 
+def print_classifications(res):
     print("======= Messages Seen =======")
-    print(f"Total processed:\t{mpProcessed:d}")
+    print(f"Total processed:\t{res.processed['MP']:d}")
     print(f"By Source and Type:")
-    for ms in mpSeen:
-        for mt in mpSeen[ms]:
-            print(f"\t0x{ms:02x}\t0x{mt:02x}\t{mpSeen[ms][mt]:d}")
+    for ms in res.classes["MP"]:
+        for mt in res.classes["MP"][ms]:
+            print(f"\t0x{ms:02x}\t0x{mt:02x}\t{res.classes['MP'][ms][mt]:d}")
 
     print("\n\n===== GPS Messages Seen =====")
-    print(f"Converted:\t\t{gpsProcessed:d}")
+    print(f"Converted:\t\t{res.processed['GPS']:d}")
     print(f"Not Converted:")
-    for uC in gpsSeen:
-        for uM in gpsSeen[uC]:
-            print(f"\t0x{uC:02x}\t0x{uM:02x}\t{gpsSeen[uC][uM]:d}")
+    for uC in res.classes["GPS"]:
+        for uM in res.classes["GPS"][uC]:
+            print(f"\t0x{uC:02x}\t0x{uM:02x}\t{res.classes['GPS'][uC][uM]:d}")
     print("\n\n===== NMEA Messages Seen ====")
-    print(f"Converted:\t\t{nmeaProcessed:d}")
+    print(f"Converted:\t\t{res.processed['NMEA']:d}")
     print(f"Not Converted:")
-    for t in nmeaSeen:
-        for m in nmeaSeen[t]:
-            print(f"\t{t:3s}\t{m:3s}\t{nmeaSeen[t][m]:d}")
+    for t in res.classes["NMEA"]:
+        for m in res.classes["NMEA"][t]:
+            print(f"\t{t:3s}\t{m:3s}\t{res.classes['NMEA'][t][m]:d}")
+
+def SLClassify():
+    logging.basicConfig(level=logging.INFO)
+    args = process_arguments()
+    inFile = open(args.file, "rb")
+    res = classify_messages(inFile)
     inFile.close()
+    print_classifications(res)
 
 if __name__ == "__main__":
-    SLDump()
+    SLClassify()

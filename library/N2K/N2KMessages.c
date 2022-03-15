920536f372a2ae53e98183b1dbba68ccc7a94ca0
Thomas Lake <t.lake@swansea.ac.uk>
2022-03-15 16:17:12 +0000

    Force width of constants during integer conversion

--- library/N2K/N2KMessages.c
+++ library/N2K/N2KMessages.c
@@ -30,8 +30,10 @@ uint16_t n2k_get_uint16(const n2k_act_message *n, size_t offset) {
 
 int32_t n2k_get_int32(const n2k_act_message *n, size_t offset) {
 	uint32_t u = n2k_get_uint32(n, offset);
-	int32_t v = (u & 0x7FFFFFFF);
-	if (u & 0x80000000) { v = -1 * ((1 << 31) - v); }
+	int32_t v = (u & 0x7FFFFFFFUL);
+	if (u & 0x80000000UL) {
+		v = -1 * ((1UL << 31) - v);
+	}
 	return v;
 }
 

SELKIE-WP7/Logger

commit ad9a4517cd568ccb4fe950ad583267d998407fa2

Ensure primary clock ID stored as integer

python/SELKIELogger/SLFiles.py
@@ -101,7 +101,7 @@ class DatFile:
         ## Cached conversion functions
         self._fields = None
         ## Primary Clock Source ID
-        self._pcs = pcs
+        self._pcs = int(pcs, 0)
         ## Source/Channel Map
         self._sm = None
         ## File data records (once parsed)
@@ -395,6 +395,7 @@ class DatFile:
 
         fields = self.prepConverters(includeTS=includeTS, force=force)
         log.debug(fields)
+        log.debug(f"Primary clock source: 0x{self._pcs:02x} [{self._sm[self._pcs]}]")
 
         self._records = []
         stack = []

python/SELKIELogger/SLMessages.py
@@ -342,6 +342,8 @@ class SLChannelMap:
         @param ix Source ID
         @returns Source object
         """
+        if not isinstance(ix, int):
+            ix = int(ix, 0)
         return self._s[ix]
 
     def __iter__(self):

commit 0d3b75767b366556bf7eee30738f352836434420

Improve clock source type conversion
Fixes bug introduced in ad9a4517cd568ccb4fe950ad583267d998407fa2 where
specifying a clock source would fail if conversion to integer had taken
place upstream.

New logic converts numeric types to integers directly and only invokes
the base detection code [i.e. int(x, 0) ] for non-numeric types.

Tested against SLConvert and SLConvertGUI

python/SELKIELogger/SLFiles.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 import numpy as np
 
+from numbers import Number
 from .SLMessages import IDs, SLMessage, SLMessageSink
 
 ## @file
@@ -101,7 +102,10 @@ class DatFile:
         ## Cached conversion functions
         self._fields = None
         ## Primary Clock Source ID
-        self._pcs = int(pcs, 0)
+        if isinstance(pcs, Number):
+            self._pcs = int(pcs)
+        else:
+            self._pcs = int(pcs, 0)
         ## Source/Channel Map
         self._sm = None
         ## File data records (once parsed)